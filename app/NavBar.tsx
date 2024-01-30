5babbbcd0cce2c64569cda7523d58cdfd507f0a0
Joe Griffin <jgriffin1410@gmail.com>
2024-01-30 05:29:34 +0000

    Radix icons added to dependencies

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Container, Flex, Text } from '@radix-ui/themes';
+import { Box, Button, Container, Flex, IconButton, Text } from '@radix-ui/themes';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const NavBar = () => {
     return (
         <nav className="p-3">
-            <Flex justify='between'>
+            <Flex justify='between' align='center'>
                 <Link href='/'>
                     <Image
                         src={'/images/qtc.png'}
@@ -17,6 +17,11 @@ const NavBar = () => {
                 <Button size="3" variant="soft">
                     HTML
                 </Button>
+                <Flex>
+                    <IconButton>
+                        
+                    </IconButton>
+                </Flex>
             </Flex>
         </nav>
     );
