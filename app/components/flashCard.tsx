bf9b4cae7a40455d091bd235455c582c5e84bf61
Joe Griffin <jgriffin1410@gmail.com>
2024-01-31 19:15:07 +0000

    static cards layered with grid

--- app/components/flashCard.tsx
+++ app/components/flashCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex } from '@radix-ui/themes';
+import { Card, Flex, Grid } from '@radix-ui/themes';
 
 interface FlashCard {
     id: number;
@@ -52,19 +52,71 @@ const htmlFlashCards: FlashCard[] = [
 
 const flashCard = () => {
     return (
+        // Master card
+        // <div className="mt-[20vh]">
+        //     <Flex justify="center" direction="column" align="center">
+        //         <Card
+        //             style={{
+        //                 width: 250,
+        //                 height: 344,
+        //                 border: '5px solid black',
+        //             }}
+        //         >
+        //             <p>
+        //                 {htmlFlashCards[0].question}
+        //             </p>
+        //         </Card>
+        //     </Flex>
+        // </div>
         <div className="mt-[20vh]">
-            <Flex justify="center" direction="column" align="center">
-                <Card
-                    style={{
-                        width: 250,
-                        height: 344,
-                        border: '5px solid black',
-                    }}
-                >
-                    <p>
-                        {htmlFlashCards[0].question}
-                    </p>
-                </Card>
+            <Flex justify='center'>
+                <div className="grid grid-cols-10 grid-rows-10 p-5 h-[400px] max-w-[390px]">
+                    <Card
+                        style={{
+                            border: '5px solid black',
+                            background: 'white',
+                        }}
+                        className="col-start-1 col-end-7 row-start-3 row-end-9 z-[9]"
+                    >
+                        <p>{htmlFlashCards[0].question}</p>
+                    </Card>
+                    <Card
+                        style={{
+                            border: '5px solid black',
+                            background: 'white',
+                        }}
+                        className="col-start-6 col-end-11 row-start-3 row-end-9 z-20"
+                    >
+                        <p>{htmlFlashCards[0].question}</p>
+                    </Card>
+                    <Card
+                        style={{
+                            border: '5px solid black',
+                            background: 'white',
+                        }}
+                        className="col-start-2 col-end-8 row-start-2 row-end-10 z-40"
+                    >
+                        <p>{htmlFlashCards[0].question}</p>
+                    </Card>
+                    <Card
+                        style={{
+                            border: '5px solid black',
+                            background: 'white',
+                        }}
+                        className="col-start-3 col-end-9 row-start-1 row-end-12 row-span-2 z-50"
+                    >
+                        <p>{htmlFlashCards[0].question}</p>
+                    </Card>
+                    <Card
+                        style={{
+                            border: '5px solid black',
+                            background: 'white',
+                        }}
+                        className="col-start-5 col-end-10 row-start-2 row-end-10 z-30"
+                    >
+                        <p>{htmlFlashCards[0].question}</p>
+                    </Card>
+                </div>
             </Flex>
         </div>
     );
