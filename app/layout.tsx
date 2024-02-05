399e5e26451bcbe77b6f51fbd978dcad0e3bcddd
Joe Griffin <jgriffin1410@gmail.com>
2024-02-05 17:42:09 +0000

    moved font to body to be passed down

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className={oxanium.className}>
-            <body>
+        <html lang="en">
+            <body className={`${oxanium.className} anitaliased`}>
                 <Theme>
                     <header>
                         <NavBar />
