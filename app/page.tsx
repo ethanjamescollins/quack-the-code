3ae3fb1694d9b27672fcd5fac27eb839fb89f4ad
Joe Griffin <jgriffin1410@gmail.com>
2024-02-05 07:13:33 +0000

    cards rendered dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Flex } from '@radix-ui/themes';
 import FlashCards from './ui/flashCards/FlashCards';
-import RatingButtons from './ui/flashCards/RatingButtons';
 
 export default function Home() {
     return (
-        <Flex direction="column" align="center">
+        <Flex direction="column" align="center" justify='center'>
             <FlashCards />
-            <RatingButtons />
         </Flex>
     );
 }
--- app/ui/flashCards/FlashCards.tsx
+++ app/ui/flashCards/FlashCards.tsx
@@ -1,5 +1,6 @@
 import { Card, Flex } from '@radix-ui/themes';
 import cardsDummyData from '@/app/lib/placeholder-data';
+import RatingButtons from './RatingButtons';
 
 interface FlashCard {
     id: number;
@@ -81,57 +82,36 @@ const FlashCards = () => {
         //         </div>
         //     </Flex>
         // </div>
+        // <Card
+        //     style={{
+        //         border: '5px solid black',
+        //         background: 'white',
+        //     }}
+        // >
+        //     <p></p>
+        // </Card>
 
         <div className="mt-[20vh]">
-            <Flex justify="center">
-                <div className="grid grid-cols-10 grid-rows-10 p-5 h-[400px] max-w-[390px]">
-                    <Card
-                        style={{
-                            border: '5px solid black',
-                            background: 'white',
-                        }}
-                        className="col-start-1 col-end-7 row-start-3 row-end-9 z-[9]"
-                    >
-                        <p></p>
-                    </Card>
-                    <Card
-                        style={{
-                            border: '5px solid black',
-                            background: 'white',
-                        }}
-                        className="col-start-6 col-end-11 row-start-3 row-end-9 z-20"
-                    >
-                        <p></p>
-                    </Card>
-                    <Card
-                        style={{
-                            border: '5px solid black',
-                            background: 'white',
-                        }}
-                        className="col-start-2 col-end-8 row-start-2 row-end-10 z-40"
-                    >
-                        <p></p>
-                    </Card>
-                    <Card
-                        style={{
-                            border: '5px solid black',
-                            background: 'white',
-                        }}
-                        className="col-start-3 col-end-9 row-start-1 row-end-12 row-span-2 z-50"
-                    >
-                        <p></p>
-                    </Card>
-                    <Card
-                        style={{
-                            border: '5px solid black',
-                            background: 'white',
-                        }}
-                        className="col-start-5 col-end-10 row-start-2 row-end-10 z-30"
-                    >
-                        <p></p>
-                    </Card>
-                </div>
-            </Flex>
+            {/* <Flex justify="center"> */}
+            <div className="carousel carousel-center bg-black rounded-box w-[80vw]">
+                {flashCardData.map(data => (
+                    <div className="carousel-item" key={data.id}>
+                        <Card
+                            style={{
+                                border: '5px solid black',
+                                background: 'white',
+                                width: '190px',
+                            }}
+                        >
+                            <p>{data.question}</p>
+                            <Flex justify="center" className="mt-[90%]">
+                                <RatingButtons />
+                            </Flex>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+            {/* </Flex> */}
         </div>
     );
 };
